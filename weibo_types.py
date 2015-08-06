00ea96e20e1dc07483dce929fa2c58293e820bd7
PegasusWang <291374108@qq.com>
2015-08-07 00:55:45 +0800

    add weibo_types

--- weibo_types.py
+++ weibo_types.py
@@ -7,29 +7,32 @@ import config
 import StringIO
 import requests
 import random
-from weibo import Client
 from crawler.qiubai_crawler import QiubaiSpider
 from single_process import single_process
 from db import leancloud_api
+from weibo_app import WeiboApp
+
 
 class WeiboTypes(object):
-    map_method = {
-        'get_gif_file': self.get_gif_file,
-        'get_qiubai_hot': self.get_qiubai_hot,
-        'get_qiubai_img': self.get_qiubai_img,
-        'get_qiubai_duanzi': self.get_qiubai_duanzi,
-    }
+
     def __init__(self, weibo_app):
         self.weibo_app = weibo_app
-
+        self.map_method = {
+            'get_gif_file': self.get_gif_file,
+            'get_qiubai_hot': self.get_qiubai_hot,
+            'get_qiubai_img': self.get_qiubai_img,
+            'get_qiubai_duanzi': self.get_qiubai_duanzi,
+        }
 
     def choose(self, type_name):
-        f = WeiboTypes.map_method.get(type_name)
-        f()
+        type_name = 'get_' + type_name
+        fun = self.map_method.get(type_name)
+        fun()
 
     def get_gif_file(self):
+        upload = leancloud_api.LeanCloudApi('ImgFile')
         d = upload.get_imgfile_by_recent_ID()
-        weibo
+        self.weibo_app.post_img(d.get('content'), d.get('pic'))
 
     def get_qiubai_hot(self):
         url = "http://m.qiushibaike.com/hot/page"
@@ -41,70 +44,27 @@ class WeiboTypes(object):
         #print i.get('content'), img_url
         print img_url
         pic = StringIO.StringIO(requests.get(img_url).content)
-        weibo_app.post_img(i.get('content'), pic)
+        self.weibo_app.post_img(i.get('content'), pic)
 
     def get_qiubai_img(self):
-        url = "http://m.qiushibaike.com/hot/page"
+        url = "http://m.qiushibaike.com/imgrank"
         s = QiubaiSpider(url)
         html = s.get_html()
-        l = s.get_hot(html)
+        l = s.get_img(html)
         i = random.choice(l)
         img_url = i.get('img')
         #print i.get('content'), img_url
         print img_url
         pic = StringIO.StringIO(requests.get(img_url).content)
-        weibo_app.post_img(i.get('content'), pic)
+        self.weibo_app.post_img(i.get('content'), pic)
 
     def get_qiubai_duanzi(self):
-        url = "http://m.qiushibaike.com/hot/page"
+        url = "http://m.qiushibaike.com/text"
         s = QiubaiSpider(url)
         html = s.get_html()
         l = s.get_hot(html)
         i = random.choice(l)
-        img_url = i.get('img')
-        #print i.get('content'), img_url
-        print img_url
-        pic = StringIO.StringIO(requests.get(img_url).content)
-        weibo_app.post_img(i.get('content'), pic)
-
-
-
-def post_weibo(weibo_app, cur_type):
-    upload = leancloud_api.LeanCloudApi('ImgFile')
-    if cur_type == 'gif':
-        d = upload.get_imgfile_by_recent_ID(50)
-        #print d.get('content')
-        weibo_app.post_img(d.get('content'), d.get('pic'))
-
-        return
-    if cur_type == 'duanzi':
-        url = "http://m.qiushibaike.com/text"
-    elif cur_type == 'hot':
-        url = "http://m.qiushibaike.com/hot/page"
-    else:
-        url = "http://m.qiushibaike.com/imgrank"
-
-    s = QiubaiSpider(url)
-    html = s.get_html()
-
-    if cur_type == 'duanzi':
-        l = s.get_duanzi(html)
-        i = random.choice(l)
-        #print i.get('content')
-        weibo_app.post_text(i.get('content'))
-
-    else:
-        if cur_type == 'hot':
-            l = s.get_hot(html)
-        else:
-            l = s.get_img(html)
-        i = random.choice(l)
-        img_url = i.get('img')
-        #print i.get('content'), img_url
-        print img_url
-        pic = StringIO.StringIO(requests.get(img_url).content)
-        weibo_app.post_img(i.get('content'), pic)
-
+        self.weibo_app.post_text(i.get('content'))
 
 def generate_type_list(d):
     """d is dict of types, {'gif':10, 'png': 3}. d[k] is weight of k"""
@@ -125,7 +85,10 @@ def test():
 
     weibo_app = WeiboApp(api_key, api_secret, callback_url, username,
                          password, uid)
-    post_weibo(weibo_app, 'gif')
+    #post_weibo(weibo_app, 'gif')
+    weibo_types = WeiboTypes(weibo_app)
+    weibo_types.choose('gif_file')
+
 
 
 @single_process
@@ -149,4 +112,5 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    #main()
+    test()
