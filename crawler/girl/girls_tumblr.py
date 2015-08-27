9e2949e448d40721782ab0186946eafd338dee1d
PegasusWang <291374108@qq.com>
2015-08-28 07:55:34 +0800

    add upload

--- crawler/girl/girls_tumblr.py
+++ crawler/girl/girls_tumblr.py
@@ -263,3 +263,9 @@ class IdolmaniaxSpider(Spider):
     def get_img(self, url='http://idolmaniax.tumblr.com/'):
         img_list = get_media_url_list(url)
         return set([i for i in img_list if 'media.tumblr' in i])
+
+
+class TokuninaidesuSpider(Spider):
+    def get_img(self, url='http://tokuninaidesu.tumblr.com/'):
+        img_list = get_media_url_list(url)
+        return set([i for i in img_list if 'media.tumblr' in i])
