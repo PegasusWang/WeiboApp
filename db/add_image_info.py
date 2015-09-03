15c0cd0b4b2921dab5f153c943b0bdf2a360c377
PegasusWang <291374108@qq.com>
2015-09-03 09:20:14 +0800

    add

---
+++ db/add_image_info.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from leancloud_api import
+if __name__ == '__main__':
+    main()
--- test/test_leancloud_api.py
+++ test/test_leancloud_api.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from db.leancloud import LeanCloudApi
+import _env
+from db.leancloud_api import LeanCloudApi
 
 
+def callback(res):
+    for i in res:
+        print i.get('ID')
 
 
+def test():
+    l = LeanCloudApi("Girls")
+    l.solve_all_class_obj(callback)
 
 
 if __name__ == '__main__':
-    main()
+    test()
