70738b588b7cd2615c8759b41c270b4d1c2980e9
Pegasus Wang <tianma201211@gmail.com>
2015-10-03 22:49:24 +0800

    change config of app

--- admin/_leancloud_init.py
+++ admin/_leancloud_init.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
+import _env
+from db import config
 import leancloud
-import leancloud_config
 
-leancloud.init(leancloud_config.LeanCloud.APP_ID,
-               master_key=leancloud_config.LeanCloud.APP_MASTER_KEY)
+leancloud.init(config.LeanCloud.WeiboApp_APP_ID,
+               master_key=config.LeanCloud.WeiboApp_APP_MASTER_KEY)
